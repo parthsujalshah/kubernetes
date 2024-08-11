3723c7c62ff5f34219933bc52785a7e93cc817f7
parth <parthsujalshah@gmail.com>
2024-08-11 17:29:46 +0530

    junits

--- src/test/java/com/kubernetes/kubernetes/KubernetesApplicationTests.java
+++ src/test/java/com/kubernetes/kubernetes/KubernetesApplicationTests.java
@@ -3,6 +3,8 @@ package com.kubernetes.kubernetes;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Collections;
+
 @SpringBootTest
 class KubernetesApplicationTests {
 
